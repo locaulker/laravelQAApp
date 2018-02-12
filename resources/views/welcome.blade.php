022e575843ef2ec366ea3a0cacfee34be78a9e02
Lawrence Caulker <locaulker@gmail.com>
2018-02-12 10:39:29 -0500

    added blade templating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
-<div class="container">
-	<div class="jumbotron">
-		<h1>Ask A Question</h1>
-		<p>Ask any question you want about laravel and we will get answers for you!</p>
-		<p>
-			<a href="#" class="btn btn-primary btn-lg">Ask Now!</a>
-		</p>
-	</div>
-	<h2>Recent Questions</h2>
-</div><!-- /.container -->
+@extends('template')
+
+@section('content')
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Ask A Question</h1>
+			<p>Ask any question you want about laravel and we will get answers for you!</p>
+			<p>
+				<a href="#" class="btn btn-primary btn-lg">Ask Now!</a>
+			</p>
+		</div>
+		<h2>Recent Questions</h2>
+	</div><!-- /.container -->
+@endsection
