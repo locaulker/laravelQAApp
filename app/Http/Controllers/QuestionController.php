05ed55eb48f2ef806a97ad85ace963c2e4d20e24
Lawrence Caulker <locaulker@gmail.com>
2018-02-13 20:50:56 -0500

    index, show and create methods added

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,82 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		// go to the model and get agroup of records
+		$questions = Question::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+		// return the view, and pass in the group of records to loop through
+		return view('questions.index')->with('questions', $questions);
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('questions.create');
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		// validate the form data
+		$this->validate($request, [
+			'title' => 'required|max:255'
+			// 'description' => ''
+		]);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+		// process the data and submit to database
+		$question = new Question();
+		$question->title = $request->title;
+		$question->description = $request->description;
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+		// if submit is successful, redirect to show method
+		if($question->save()) {
+			return redirect()->route('questions.show', $question->id);
+		} else {
+			return redirect()->route('questions.create');
+		}
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		// use the model to get 1 record from the database
+		$question = Question::findOrFail($id);
+
+		// show the form for editing the specified resource
+		return view('questions.show')->with('question', $question);
+
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,16 @@
+@extends('template')
+
+@section('content')
+  <div class="container">
+    <h1>Recents Questions</h1>
+    <hr>
+
+    @foreach ($questions as $question)
+      <div class="well">
+        <h3>{{ $question->title }}</h3> 
+      <p>{{ $question->description }}</p>
+      <a href="{{ route('questions.show', $question->id) }}" class="btn btn-primary btn-small">View Details</a>
+    </div>
+    @endforeach
+  </div>
+@endsection
